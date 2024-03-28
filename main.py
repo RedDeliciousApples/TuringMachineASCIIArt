e54525dc9b182cb6e5d58e7bbf25933a5493037f
csal <danishbuttercookies@duck.com>
2024-03-27 21:58:59 -0700

    pep 8 reformat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from typing import *
 
 
-
 class TuringMachine:
     def __init__(self, tape: List[int], initial_state: str, state_machine: Dict[Tuple[str, int], Tuple[str, int, str]],
                  pos: int, end_state: str):
@@ -12,7 +11,8 @@ class TuringMachine:
         self.end_state = end_state
 
     def greet(self):
-        return f"Starting with tape: {self.tape}\n and state {self.state}\n with state table \n {self.state_table}\n at index {self.pos}\n to reach state {self.end_state}."
+        return (f"Starting with tape: {self.tape}\n and state {self.state}\n with state table \n {self.state_table}\n"
+                f" at index {self.pos}\n to reach state {self.end_state}.")
 
     def step(self) -> bool:
         current_symbol = self.tape[self.pos]
@@ -41,28 +41,26 @@ class TuringMachine:
 def art(tape: List[int], pos: int):
     tape_length = len(tape)
 
-    offset = ("  " * pos)
-
     print("-" * (tape_length * 10))
 
     # Note: there are 5 spaces
 
     print("     |     |" * tape_length)
     for i in range(pos, tape_length):
-        print(f"     |  {tape[i]}  |", end = "")
-    #empty print statement for 1 newline
+        print(f"     |  {tape[i]}  |", end="")
+    # empty print statement for 1 newline
     print("")
     print("     |     |" * tape_length)
 
-
-    print("-" * (tape_length*10))
+    print("-" * (tape_length * 10))
 
     arrow(0)
 
-def arrow(pos : int):
+
+def arrow(pos: int):
     # number of spaces on left: 8, 6, 4, 6, 6, 6
     # for simplicity, we use an offset of 6 spaces
-    #12 spaces between centers of each cell
+    # 12 spaces between centers of each cell
     offset: str = "      " + ((" " * 12) * pos)
     # This adds 2 spaces
     print(f"{offset + '  '}.   ")
@@ -73,6 +71,7 @@ def arrow(pos : int):
     print(f"{offset};;;;;   ")
     print(f"{offset};;;;;")
 
+
 # Format: {If at (state, symbol), do (new_state, new_symbol, move)
 # Our symbols are ints, but could be anything
 states = {
@@ -81,7 +80,7 @@ states = {
     ('s1', 0): ('s0', 1, 'R'),
     ('s1', 1): ('s1', 0, 'R'),
 }
-initial_tape = [0, 1, 1, 0,0,1,1,0,0,1,1,0,1]
+initial_tape = [0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1]
 
 if __name__ == "__main__":
     machine = TuringMachine(initial_tape, 's0', states, 0, "s1")
